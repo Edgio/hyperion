c5affb72ba9e553ed822feaf5f3cd0c8db4b2050
davebear <dandrews@edgecast.com>
2023-10-19 22:38:37 -0700

    Starting to add some of the specs in here

--- bin/hyperion_linter.py
+++ bin/hyperion_linter.py
@@ -40,19 +40,20 @@ def verbose(a_msg):
 
 
 
-class hyperion_error:
-    def __init__(self, a_section, a_line, a_msg):
+class hyperion_finding:
+
+    TYPE_ERROR=1
+    TYPE_WARNING=2
+
+    def __init__(self, a_section, a_path,
+                 a_msg, a_type = TYPE_ERROR):
+        self.type = a_type
         self.section = a_section
-        self.line = a_line
+        self.path = a_path
         self.msg = a_msg
 
 
 class hyperion_linter:
-
-    # // Static Variables
-    LC_SERVERS = {"EC": "api-int.edgecast.com", # the default server to connect to
-                  "IR500": "api-int.transactcdn.com",
-                  "QA": "qa-uber.edgecast.com"}
 
 
     # // Functions
@@ -71,7 +72,8 @@ class hyperion_linter:
         self.sections = a_sections
         self.file = open(a_file)
         self.spec = yaml.load(self.file, Loader=yaml.FullLoader)
-
+        #verbose("Spec:")
+        #verbose(self.spec)
 
     # /// \brief   Lint some shit
     # /// \details Run all configured linters, print results from all failures
@@ -109,14 +111,48 @@ class hyperion_linter:
 
         #verbose("Errors: '%s'" % errors)
         for error in errors:
-            print("Section: %s   Line: %d:  %s" % (error.section, error.line, error.msg))
+            print("%s Section: %s   Path: %s:  %s" % ("WARN" if error.type == hyperion_finding.TYPE_WARNING else "ERR", error.section, error.path, error.msg))
 
 
     # /// \brief   Lint how paths are specified
     # /// \details
     # /// \return  List of hyperion_error objects identified or None
     def lint_paths(self):
-        return []
+        retval = []
+        if ("servers" not in self.spec or
+            len(self.spec["servers"]) == 0 or
+            "url" not in self.spec["servers"][0]):
+            # definitely failed
+            retval.append(hyperion_finding("paths", 0, "servers[0].url doesn't exist"))
+        else:
+            url = self.spec["servers"][0]["url"]
+
+            # must be relative
+            if url.startswith("/"):
+                retval.append(hyperion_finding("paths", "servers[0].url", "starts with '/', but must be a relative path"))
+
+            # must use - for words in path
+            if "_" in url:
+                retval.append(hyperion_finding("paths", "servers[0].url", "uses '_' as a separator.  Must use '-'"))
+
+            # should lowercase chars
+            if not url.islower():
+                retval.append(hyperion_finding("paths", "servers[0].url", "uses uppercase chars" % url, hyperion_finding.TYPE_WARNING))
+
+        for path_id in self.spec["paths"]:
+            path_val = self.spec["paths"][path_id]
+            verbose("Analyzing path: '%s' -> '%s'" % (path_id, path_val))
+
+            # must use - for words in path
+            if "_" in path_id:
+                retval.append(hyperion_finding("paths", "path.%s" % path_id, "uses '_' as a separator.  Must use '-'"))
+
+            # must use snake_case for qs
+
+            # should lowecase chars
+            if not path_id.islower():
+                retval.append(hyperion_finding("paths", "paths.%s" % path_id, "uses uppercase chars", hyperion_finding.TYPE_WARNING))
+        return retval
 
     # /// \brief   Lint how versioning is implemented
     # /// \details
@@ -134,6 +170,14 @@ class hyperion_linter:
     # /// \details
     # /// \return  List of hyperion_error objects identified or None
     def lint_status_codes(self):
+        allowed_status_codes = [200, 201, 202, 204,
+
+                                301, 303, 304, 307, 308,
+
+                                400, 401, 403, 404, 405,
+                                409, 413, 422, 429,
+
+                                500, 502, 503, 504]
         return []
 
     # /// \brief   Lint how dates are represented
@@ -159,7 +203,6 @@ class hyperion_linter:
     # /// \return  List of hyperion_error objects identified or None
     def lint_document_structure(self):
         retval = []
-        retval.append(hyperion_error("document-structure", 1, "test error"))
         return retval
 
 
